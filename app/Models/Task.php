467c1b88b4e08b4b3b06631ae5b8aa8c0dc2420f
valenace <valenacego8@gmail.com>
2024-02-01 22:52:16 -0400

    Migration, taks, rel with user

---
+++ app/Models/Task.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;
+class Task extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'status',
+        'priority',
+        'deadline',
+    ];
+
+    public function user():BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+    
+}
