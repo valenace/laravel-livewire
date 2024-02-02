08fe4c39db4ec7e262d858cda4fd635d94797d98
valenace <valenacego8@gmail.com>
2024-02-01 21:55:27 -0400

    Nav

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
-                <livewire:welcome.navigation />
+                <livewire:auth.navigation />
             @endif
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
